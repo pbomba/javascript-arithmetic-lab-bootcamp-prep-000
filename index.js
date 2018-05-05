836c40af8fe3bb4fb8c2b0f8db1bead1477e2054
Paul Bomba` <paul@paulbomba.com>
2018-05-05 19:04:56 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,32 @@
+let a = 2;
+let b = 3;
+let n;
+
+function add(a,b) {
+  return a + b;
+}
+
+function subtract (a,b){
+  return a - b;
+}
+
+function multiply (a, b){
+  return a * b;
+}
+
+function divide (a, b){
+  return a / b;
+}
+
+function inc(n){
+  n++;
+  return n;
+}
+
+function dec(n){
+  n--;
+  return n;
+}
+function makeInt(n){
+ return +n; 
+}
